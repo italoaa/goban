10d519e956c91ee648ed492471fa93d6edf2f18e
Italo Amaya <italo.amaya@openchip.com>
2026-08-27 11:28:29 +0200

    kill this buffer

--- init.el
+++ init.el
@@ -32,7 +32,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("138ed99a323c1b93c52f4b3726caf2bc634b79a76fa63a3d3aff76394db5f28f"
+   '("300c9134f40efe3702f3de33c390770c805cd6caa5f1384496208da886e4b652"
+     "384e771aae814b73e18d69b057af9707247962a2c8307e0c6232d001f9962ff5"
+     "138ed99a323c1b93c52f4b3726caf2bc634b79a76fa63a3d3aff76394db5f28f"
      "2493d0ad0bb94bd2ad297a6d76288751a532fd6d8d6af694ac14008caa6b7fa2"
      "530e730924892af285af79d88339048da48c572a3c974882682eadb9881fb051"
      "0d7a8edff6a900b5a642f9014d394661c377baeaaab64f2b62fc70701bb12b18"
